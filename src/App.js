8432f19ebc9716beec71a6519851e54cece05bc5
unknown <alex.rocha@dtidigital.com.br>
2021-05-05 13:42:20 -0300

    listando cards dos filmes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import './App.css'
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
 
@@ -19,9 +21,7 @@ export default () => {
     <div className="page">
       <section className="lists">
         {movieList.map((item, key) => (
-          <div>
-            {item.title}
-          </div>
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
