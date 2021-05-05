a7436d5a1cd43c2560c1a99866ef6f1abce33c2c
unknown <alex.rocha@dtidigital.com.br>
2021-05-05 11:32:49 -0300

    listando categorias

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
 export default () => {
 
+  const [movieList, setMovieList] = useState([]);
+
   //executa codigo escrito ao carregar pagina
-  useEffect(() =>{
-    const loadAll = async() => {
-        // Pegando a lista TOTAL
-        let list = await Tmdb.getHomeList();
-        console.log(list)
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
     }
     loadAll();
-  },[]);
+  }, []);
 
   return (
-    <div>Ola Mundo!</div>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </section>
+    </div>
   )
 }
